018a6c49d7498047b989d8493ded1e3cf08cdabe
Christian Schmidt <cjschmidt33@gmail.com>
2024-11-15 10:57:48 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import './ProductList/jsx'
+import './AddProductForm.jsx'
+
+const products = [
+    { id: 1, name: 'Basketball', price: 60, description: `An icon with unmatched feel, no matter how many imitators come along. Walk into any gym and odds are you'll find people playing with a Wilson Evolution.` },
+    { id: 2, name: 'Jersey', price: 130, description: `Take a step back in time to the 90's with the Cade Cunningham Swingman Jersey from Nike.` },
+    { id: 3, name: 'Shoes', price: 120, description: `A unique rubber outsole is designed to generate traction where you need it most, so every hard-charging cut and head fake has total support.` },
+    { id: 4, name: 'Compression Shorts', price: 30, description: `While preparing for the season, basketball compression shorts have your back. They're stretchy, comfy, and full of Sport Style.` },
+  ];
+
+export default products
